c73b71661c29fae20a0d38db4c0cc39482bc48d0
ASekenov <a.sekenov@p-s.kz>
2020-04-06 20:05:56 +0600

    dd

--- src/course/course.module.ts
+++ src/course/course.module.ts
@@ -16,23 +16,10 @@ import { UserModule } from '../user/user.module';
   ]
 })
 export class CourseModule implements NestModule {
-  configure(consumer: MiddlewareConsumer) {
-    // throw new Error("Method not implemented.");
-    console.log("d");
+  public configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(AuthMiddleware)
+      .forRoutes(
+        {path: 'course/create', method: RequestMethod.POST})
   }
-  // public configure(consumer: MiddlewareConsumer) {
-  //   consumer
-  //     .apply(AuthMiddleware)
-  //     .forRoutes(
-  //       {path: 'courses', method: RequestMethod.POST},
-  //       {path: 'courses/delete', method: RequestMethod.POST},
-  //       {path: 'courses/zaebal', method: RequestMethod.GET},
-  //       {path: 'courses/course/:id', method: RequestMethod.GET},
-  //       {path: 'courses/:title', method: RequestMethod.GET},
-  //       {path: 'courses/:slug', method: RequestMethod.PUT},
-  //       {path: 'courses/:slug/comments', method: RequestMethod.POST},
-  //       {path: 'courses/:slug/comments/:id', method: RequestMethod.DELETE},
-  //       {path: 'courses/:slug/favorite', method: RequestMethod.POST},
-  //       {path: 'courses/:slug/favorite', method: RequestMethod.DELETE});
-  // }
 }
